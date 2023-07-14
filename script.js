d8d2336429a2d2179b85a7e06d4f2a8ea9efff26
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-14 13:54:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+	if(num == 1) return 0;
+	if(num == 2) return 1;
+	int prev1 = 0;
+	int prev2 = 1;
+	for(int i = 3;i<=num;i++)
+	{
+		int curr = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = curr;
+	}
+	return prev2;
 }
 
 module.exports = fibonacci;
